5c024ae7078fd7970a5287b72679112644f0a992
Abdullah2213565 <dulakhan024@gmail.com>
2022-07-26 15:16:22 +0200

    Index.js update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './styles/style.css';
-import './styles/bootstrap.min.css';
-import './styles/bootstrap-grid.min.css';
-import './bootstrap.bundle.min.js';
 import displayShow from './renderDOM.js';
 
 document.addEventListener('DOMContentLoaded', () => {
