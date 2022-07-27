49905c290b38a947d016f3301059eec64fff7fee
Abdullah2213565 <dulakhan024@gmail.com>
2022-07-27 11:19:30 +0200

    Update renderDOM.js

--- src/renderDOM.js
+++ src/renderDOM.js
@@ -28,7 +28,7 @@ const renderNavbar = (results) => {
   // Creating navigation bar
   const navBar = `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">TVShows</a>
+    <a class="navbar-brand" href="#">TVShows logo</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
