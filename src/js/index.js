fadfde16a2b7da6d8171df9beea8d172892062c4
Abdullah2213565 <dulakhan024@gmail.com>
2022-07-29 15:13:56 +0200

    Fixing errors

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,11 @@ import '../styles/style.css';
 import '../styles/bootstrap.min.css';
 import '../styles/bootstrap-grid.min.css';
 import '../bootstrap.bundle.min.js';
-import addEventToCommentBtn from '../comment.js'
+import addEventToCommentBtn from '../comment.js';
 import Img from '../no_image.jpg';
 import {
   renderNavbar, renderShow, displayLikes,
 } from './renderDOM.js';
-
 
 document.addEventListener('DOMContentLoaded', async () => {
   await renderNavbar();
--- src/mocks/commentCount.js
+++ src/mocks/commentCount.js
@@ -1,20 +1,20 @@
 const commentCard = (commentObj) => {
-    const element = `
+  const element = `
         <li>
           <span class="comment-date">${commentObj} </span>
          
         </li>`;
-    document.querySelector('.comments-container').insertAdjacentHTML('beforeend', element);
-  };
-  
-  const getCommentCount = () => {
-    const container = document.querySelector('.comments-container');
-    const count = container.children.length;
-    return count;
-  };
-  const updateCommentCount = () => {
-    const commentCountEl = document.querySelector('.comment-count');
-    commentCountEl.textContent = `(${getCommentCount()})`;
-  };
-  
-  export { updateCommentCount, commentCard };
+  document.querySelector('.comments-container').insertAdjacentHTML('beforeend', element);
+};
+
+const getCommentCount = () => {
+  const container = document.querySelector('.comments-container');
+  const count = container.children.length;
+  return count;
+};
+const updateCommentCount = () => {
+  const commentCountEl = document.querySelector('.comment-count');
+  commentCountEl.textContent = `(${getCommentCount()})`;
+};
+
+export { updateCommentCount, commentCard };
